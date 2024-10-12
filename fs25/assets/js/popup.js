960355f9355588306ac9fbf4ca4b6cc1b3a7d089
BySwiizen <byswiizen@gmail.com>
2024-10-13 01:52:25 +0200

    Add "fs25"

---
+++ fs25/assets/js/popup.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+document.querySelector("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
